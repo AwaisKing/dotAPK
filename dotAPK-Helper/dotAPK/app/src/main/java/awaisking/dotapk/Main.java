d0f306f09528f2708523413d03a90a84d1652e93
Awais Soomro <awaisking.magician@gmail.com>
2016-11-06 16:11:07 +0500

    dotAPK Helper - v2.0
    Fixed low-resolution icons! :D

--- dotAPK-Helper/dotAPK/app/src/main/java/awaisking/dotapk/Main.java
+++ dotAPK-Helper/dotAPK/app/src/main/java/awaisking/dotapk/Main.java
@@ -1,15 +1,15 @@
 package awaisking.dotapk;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -17,20 +17,24 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 public class Main extends Activity {
+    String STOP_CMD = "awaisking.dotapk.STOP";
+
+    BroadcastReceiver killerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            try {
+                unregisterReceiver(killerReceiver);
+            } catch (Exception ignored) {}
+            Main.this.finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +43,29 @@ public class Main extends Activity {
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
         List<ResolveInfo> pkgAppsList = getPackageManager().queryIntentActivities(mainIntent, 0);*/
 
+        registerReceiver(killerReceiver, new IntentFilter(STOP_CMD));
+
         try {
             doMagicStuff();
-        } catch (PackageManager.NameNotFoundException e) {}
+        } catch (PackageManager.NameNotFoundException ignored) {}
+    }
 
-        //Toast.makeText(this, "DONE!!", Toast.LENGTH_SHORT).show();
-        //moveTaskToBack(true);
-        //finish();
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            unregisterReceiver(killerReceiver);
+        } catch (Exception ignored) {}
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            unregisterReceiver(killerReceiver);
+        } catch (Exception ignored) {}
+        finish();
     }
 
     @SuppressWarnings("Deprecated")
@@ -58,20 +78,21 @@ public class Main extends Activity {
                 int displayMetrics[] = {DisplayMetrics.DENSITY_XHIGH, DisplayMetrics.DENSITY_HIGH, DisplayMetrics.DENSITY_TV};
                 for (int displayMetric : displayMetrics) {
                     try {
+                        @SuppressWarnings("deprecation")
                         Drawable d = otherAppCtx.getResources().getDrawableForDensity(pi.applicationInfo.icon, displayMetric);
                         if (d != null) return d;
                     } catch (Resources.NotFoundException e) {
 //                      Log.d(TAG, "NameNotFound for" + packageName + " @ density: " + displayMetric);
                     }
                 }
-            } catch (Exception ignored) {
-            }
+            } catch (Exception ignored) {}
         }
 
         ApplicationInfo appInfo;
         try {
             appInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
         } catch (PackageManager.NameNotFoundException e) {
+            setResult(RESULT_CANCELED);
             return null;
         }
 
@@ -81,29 +102,45 @@ public class Main extends Activity {
     private void doMagicStuff() throws PackageManager.NameNotFoundException {
         PackageManager pm = getPackageManager();
         List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+        String packagesInfo = "";
         for (int i=0; i<packages.size(); i++) {
             if (packages.get(i).sourceDir.contains("/system") || packages.get(i).publicSourceDir.contains("/system")) continue;
-//            if (!packages.get(i).packageName.contains("cleverbot")) continue;
 
             Bitmap bitmap = drawableToBitmap(getIconFromPackageName(packages.get(i).packageName));
+            String encodedBitmap = encodeToBase64(bitmap);
+            encodedBitmap = encodedBitmap.replace("\r", "").replace("\n", "");
             String youKnowMe = "AWAISKING:DOTAPK:APP{" +
-                    packages.get(i).loadLabel(getPackageManager()) + ","
-                    + packages.get(i).packageName + ","
-                    + getPackageManager().getPackageInfo(packages.get(i).packageName, 0).versionName + ","
-                    + packages.get(i).targetSdkVersion + ","
-                    + "[" + packages.get(i).sourceDir + "|" + packages.get(i).publicSourceDir + "]" + ","
-                    + encodeToBase64(bitmap) + "}";
-            Log.d("AWAISKING_APP", "" + youKnowMe);
+               /*0*/  packages.get(i).loadLabel(getPackageManager()) + ","
+               /*1*/  + packages.get(i).packageName + ","
+               /*2*/  + new File(packages.get(i).publicSourceDir).length() + ","
+               /*3*/  + getPackageManager().getPackageInfo(packages.get(i).packageName, 0).versionName + ","
+               /*4*/  + packages.get(i).targetSdkVersion + ","
+               /*5*/  + "[" + packages.get(i).sourceDir + "|" + packages.get(i).publicSourceDir + "]" + ","
+               /*6*/  + encodedBitmap + "}";
+
+            packagesInfo += youKnowMe + "\r\n";
+            //Log.d("AWAISKING_APP", "" + youKnowMe);
         }
+        try {
+            @SuppressWarnings("deprecation")
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("dotAPK.txt", Context.MODE_WORLD_READABLE));
+            outputStreamWriter.write(packagesInfo);
+            outputStreamWriter.close();
+        } catch (Exception e) {
+            setResult(RESULT_CANCELED);
+        }
+        // finish();
+        setResult(RESULT_OK);
     }
 
     private String encodeToBase64(Bitmap image) {
         ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
-        Bitmap carl;
-        Log.d("ARZAC", "" + image.getHeight());
-        if (image.getHeight() > 54) carl = Bitmap.createScaledBitmap(image, (int) (image.getHeight() / 2f), (int) (image.getWidth() / 2f), false);
-        else carl = image;
-        carl.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOS);
+//        Bitmap carl;
+//        Log.d("ARZAC", "" + image.getHeight());
+//        if (image.getHeight() > 54) carl = Bitmap.createScaledBitmap(image, (int) (image.getHeight() / 2f), (int) (image.getWidth() / 2f), false);
+//        else carl = image;
+        image.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOS);
 //        Log.d("AWAISKING_APP", "imgH: " + image.getHeight() + " -- carlH: "+ carl.getHeight());
         try { return new String(Base64.encode(byteArrayOS.toByteArray(), Base64.DEFAULT), "UTF-8"); } catch (Exception e) { return "<ERROR>"; }
     }
@@ -123,6 +160,4 @@ public class Main extends Activity {
         drawable.draw(canvas);
         return bitmap;
     }
-
-
 }
