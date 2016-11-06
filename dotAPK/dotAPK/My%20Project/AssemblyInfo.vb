f2ffb855fdb309e9d2b79d5364627907d94f259d
Awais Soomro <awaisking.magician@gmail.com>
2016-11-06 16:52:12 +0500

    source changes - dotAPK v3.0
    + fixed Get Installed Apps code.
    + fixed issues with Installed Apps' icons.
    + added Remove lower API(s) apps from Duplicates dialog.

---
+++ dotAPK/dotAPK/My%20Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("dotAPK")> 
+<Assembly: AssemblyDescription("A tool for managing and installing batch APKs.")> 
+<Assembly: AssemblyCompany("Awais Soomro")> 
+<Assembly: AssemblyProduct("dotAPK")> 
+<Assembly: AssemblyCopyright("Copyright © Awais Soomro")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0d93e76f-53e4-4d14-90ba-bdf742cda503")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("3.0.0.0")> 
+<Assembly: AssemblyFileVersion("3.0.0.0")> 
